8ec0bc6b5a3773dd6681e0b963bb72bb0a376cff
Tarmo Talu <tarmo.talu@wisesoft.ee>
2010-02-21 21:10:32 +0200

    Added user roles model. Defined role codes.

--- db/migrate/20100221154928_create_roles.rb
+++ db/migrate/20100221154928_create_roles.rb
@@ -1,16 +1,25 @@
 class CreateRoles < ActiveRecord::Migration
   def self.up
     create_table :roles do |t|
-      t.string :code
-      t.references :user
-      t.references :model, :polymorphic => true, :references => nil
+      t.string :code, :null => false
 
       t.datetime :deleted_at
       t.timestamps
       t.userstamps(true)
     end
+    add_index :roles, [:code, :deleted_at], :name => 'index_roles_uk', :unique => true
     
-    add_index :roles, [:code, :user_id, :model_type, :model_id, :deleted_at], :unique => true    
+    # guest - autentimata kasutaja
+    # user - autenditud kasutaja
+    # system_administrator - süsteemi administraator
+    # account_manager - konto omanik ja registrite haldur
+    # regional_manager - haldusala administraator
+    # event_manager - kindla talgu sündmuse administraator
+    # event_participant - talgu osalemise registreerija
+    
+    %w{guest user system_administrator account_manager regional_manager event_manager event_participant}.each do |role|
+      Role.create! :code => role
+    end
   end
 
   def self.down
---
+++ db/migrate/20100221184247_create_user_roles.rb
@@ -0,0 +1,22 @@
+class CreateUserRoles < ActiveRecord::Migration
+  def self.up
+    create_table :user_roles, :force => true do |t|
+      t.references :role, :null => false
+      t.references :user, :null => false
+      t.references :model, :polymorphic => true, :references => nil
+
+      t.datetime :valid_from
+      t.datetime :valid_to
+      
+      t.datetime :deleted_at
+      t.timestamps
+      t.userstamps(true)
+      t.references :account, :null => false
+    end
+    add_index :user_roles, [:account_id, :role_id, :user_id, :model_type, :model_id, :deleted_at], :name => 'index_user_roles_uk', :unique => true
+  end
+
+  def self.down
+    drop_table :user_roles
+  end
+end
