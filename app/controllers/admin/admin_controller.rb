0e6f43883c7ab5ea8a392dda403f73672a418426
Priit Haamer <priit@fraktal.ee>
2010-02-23 12:46:48 +0200

    User interface update for administrators area.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,4 +3,6 @@ class Admin::AdminController < ApplicationController
   before_filter :require_user
 
   layout 'admin'
+  
+  helper 'admin/admin'
 end
--- app/models/settlement.rb
+++ app/models/settlement.rb
@@ -1,6 +1,10 @@
 class Settlement < ActiveRecord::Base
   
   acts_as_scoped :account
+
+  KIND = [:alevik, :alev, :kyla, :linnaosa, :vallasisene_linn]
   
   belongs_to :municipality
+  
+  validates_presence_of :name, :kind, :municipality
 end
