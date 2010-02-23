311b768343b7e289114347055a04ef6811d12114
Priit Haamer <priit@fraktal.ee>
2010-02-23 19:20:53 +0200

    Integrated public design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   filter_resource_access
   
   def index
-    @events = Event.published.all(:select => 'name, url, latitude, longitude')
+    @events = Event.all(:select => 'name, url, latitude, longitude')
     @latest = Event.published.latest(5).all(:select => 'name, url')
   end
 end
