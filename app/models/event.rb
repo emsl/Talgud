7b6813fa13ba61c054177a1826fbd4dc403d6625
Tarmo Talu <tarmo.talu@wisesoft.ee>
2010-02-25 18:42:14 +0200

    Event, role validators spec.

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,6 +20,9 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :name, :code, :url, :begins_at, :ends_at, :event_type, :manager, :status, :location_address_country_code, :location_address_county, :location_address_municipality, :max_participants
   validates_numericality_of :max_participants, :greater_than => 0, :only_integer => true
+  validates_each :ends_at  do |record, attr, value|
+    record.errors.add attr, 'peab olema tulevikus' unless record.begins_at < value
+  end 
 
   named_scope :published, :conditions => {:status => ['published', 'registration_open', 'registration_closed']}
   named_scope :my_events, lambda { |u| {:include => :roles, :conditions => {:roles => {:user_id => u, :role => Role::ROLE[:event_manager]}} }}
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,13 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Event, 'validations' do
-  it 'should validate that begin date is before end date'
+  it 'should validate that begin date is before end date' do
+    Factory.build(:event, :begins_at => 1.day.ago, :ends_at => 0.days.ago).should be_valid
+    Factory.build(:event, :begins_at => 1.day.ago, :ends_at => 2.days.ago).should be_invalid
+    Factory.build(:event, :begins_at => 1.day.ago, :ends_at => 1.days.ago).should be_valid
+  end
   
-  it 'should validate that number of participants is a positive number'
+  it 'should validate that number of participants is a positive number' do
+    Factory(:event, :max_participants => 1).should be_valid
+    Factory.build(:event, :max_participants => 0).should be_invalid
+    Factory.build(:event, :max_participants => -1).should be_invalid
+  end  
 end
 
 describe Event, 'create' do
   it 'should associate event manager role to creator of the event' do
-    
+    @event = Factory(:event)
+    @event.roles.should_not be_nil
+    @event.roles.collect(&:role).should include(Role::ROLE[:event_manager])
   end
 end
