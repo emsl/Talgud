eeeb8f52de664ceb28ecc821af71a401d443efcf
Priit Haamer <priit@fraktal.ee>
2010-03-01 17:57:49 +0200

    User interface polishing. Added latest events view and improvements to home page.

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -2,7 +2,7 @@ authorization do
   role :guest do
     has_permission_on [:home], :to => [:read]
     has_permission_on [:user_sessions, :admin_user_sessions], :to => [:manage]
-    has_permission_on [:events], :to => [:read, :map] do 
+    has_permission_on [:events], :to => [:read, :map, :latest] do 
       if_attribute :status => is { Event::STATUS[:published] }
       if_attribute :status => is { Event::STATUS[:registration_opened] }
       if_attribute :status => is { Event::STATUS[:registration_closed] }
