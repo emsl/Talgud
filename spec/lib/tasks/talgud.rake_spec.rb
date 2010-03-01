559016369e844c0dbd2e473e6c413815f4dd865e
Tarmo Talu <tarmo.talu@wisesoft.ee>
2010-03-01 20:38:48 +0200

    Talgud rake task with account manager creation and rake rspec's.

---
+++ spec/lib/tasks/talgud.rake_spec.rb
@@ -0,0 +1,60 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require 'rake'
+
+describe "talgud rake tasks" do
+  before do
+    @rake = Rake::Application.new
+    Rake.application = @rake
+    Rake.application.rake_require "lib/tasks/talgud"
+    Rake::Task.define_task(:environment)
+  end
+
+  describe "rake talgud:account:create" do
+    before do
+      @task_name = "talgud:account:create"
+    end
+    
+    it "should have 'environment' as a prereq" do
+      @rake[@task_name].prerequisites.should include("environment")
+    end
+    
+    it "should create new account and user and grant account_manager role to that user" do
+      account = Factory.build(:account)
+      user = Factory.build(:user)
+      Account.find_by_domain(account.domain).should be_nil
+      User.find_by_email(user.email).should be_nil
+      @rake[@task_name].invoke(account.name, account.domain, user.email, user.password)
+      account = Account.find_by_domain(account.domain)
+      account.should_not be_nil
+      account.domain.should eql(account.domain)
+      account.name.should eql(account.name)
+      user = User.find_by_email(user.email)
+      user.should_not be_nil
+      user.active?.should be_true
+      Role.has_role?(Role::ROLE[:account_manager], user, account).should be_true
+    end
+
+    it "should not fail on existing account and create new user and grant account_manager role to that user" do
+      account = Factory(:account)
+      user = Factory.build(:user)
+      Account.find_by_domain(account.domain).should_not be_nil
+      User.find_by_email(user.email).should be_nil
+      @rake[@task_name].invoke(account.name, account.domain, user.email, user.password)
+      user = User.find_by_email(user.email)
+      user.should_not be_nil
+      user.active?.should be_true
+      Role.has_role?(Role::ROLE[:account_manager], user, account).should be_true
+    end
+
+    it "should activate existing user and grant account_manager role" do
+      account = Factory(:account)
+      user = Factory(:user, :status => User::STATUS[:created])
+      Account.find_by_domain(account.domain).should_not be_nil
+      User.find_by_email(user.email).should_not be_nil
+      @rake[@task_name].invoke(account.name, account.domain, user.email, user.password)
+      user = User.find_by_email(user.email)
+      user.active?.should be_true
+      Role.has_role?(Role::ROLE[:account_manager], user, account).should be_true
+    end
+  end
+end
