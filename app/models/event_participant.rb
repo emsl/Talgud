51adcfb25ef2b76b52c6d6a9ca42b847eb00f8a6
Tarmo Talu <tarmo.talu@wisesoft.ee>
2010-03-11 10:39:37 +0200

    Event participiants phone.

--- app/models/event_participant.rb
+++ app/models/event_participant.rb
@@ -3,4 +3,11 @@ class EventParticipant < ActiveRecord::Base
   acts_as_scoped :account
   
   default_scope :conditions => {:deleted_at => nil}
+  
+  belongs_to :event
+  belongs_to :account
+  
+  def parent?
+    self.event_participant.nil?
+  end
 end
