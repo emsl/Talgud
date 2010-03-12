8dbe27e62f241f6ee0c2606c236d624cf2a01b8a
Priit Haamer <priit@fraktal.ee>
2010-03-12 13:19:29 +0200

    Made all specs pass

--- spec/controllers/participations_controller_spec.rb
+++ spec/controllers/participations_controller_spec.rb
@@ -1,13 +1,37 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ParticipationsController, 'create' do
-  it 'should create event participation record when submitted data is valid'
+  before(:each) do
+    @event = Factory(:event)
+    @ep = Factory.build(:event_participant)
+  end
   
-  it 'should send e-mail to all registered participants after successful registration'
+  it 'should create event participation record when submitted data is valid' do
+    post :create, {:event_id => @event.url, :event_participant => @ep.attributes}
+    response.should redirect_to(event_path(@event))
+  end
   
-  it 'should send e-mail to event managers after successful registration'
+  it 'should send e-mail to all registered participants after successful registration' do
+    Mailers::EventMailer.should_receive(:deliver_participant_notification)
+    post :create, {:event_id => @event.url, :event_participant => @ep.attributes}
+  end
   
-  it 'should send e-mail to participant friends if their e-mails are supplied'
+  it 'should send e-mail to event managers after successful registration' do
+    Role.grant_role(Role::ROLE[:event_manager], Factory(:user), @event)
+    Mailers::EventMailer.should_receive(:deliver_manager_participation_notification).twice
+    post :create, {:event_id => @event.url, :event_participant => @ep.attributes}
+  end
   
-  it 'should show register screen when registration info is invalid'
+  it 'should send e-mail to participant friends if their e-mails are supplied' do
+    @ep.tellafriend_emails = 'a@example.com,b@example.com,c@example.com'
+    Mailers::EventMailer.should_receive(:deliver_tell_friend_notification).exactly(3).times
+    post :create, {:event_id => @event.url, :event_participant => @ep.attributes}
+  end
+  
+  it 'should show register screen when registration info is invalid' do
+    @ep.firstname = nil
+    @ep.lastname = nil
+    post :create, {:event_id => @event.url, :event_participant => @ep.attributes}
+    response.should render_template(:new)
+  end
 end
