c22c2c2ecf220a5c200f5fc396497d770f2b1744
Tarmo Talu <tarmo.talu@wisesoft.ee>
2010-03-17 11:26:29 +0200

    Moved CSV rendering into view.

--- app/controllers/admin/event_participants_controller.rb
+++ app/controllers/admin/event_participants_controller.rb
@@ -9,8 +9,8 @@ class Admin::EventParticipantsController < Admin::AdminController
     respond_to do |format|
       format.html { @event_participants = @search.paginate(:page => params[:page]) }
       format.xml  { render :xml => @search.all }
-      format.csv { @event_participants = @search.all; render_csv("event-participans-#{@event.code}-#{Time.now.strftime("%Y%m%d")}") }
-      format.xls { @event_participants = @search.all}
+      format.csv { @event_participants = @search.all; @filename = "event-participans-#{@event.code}-#{Time.now.strftime("%Y%m%d")}.csv" }
+      format.xls { @event_participants = @search.all; @filename = "event-participans-#{@event.code}-#{Time.now.strftime("%Y%m%d")}.xls"}
     end
   end
 
@@ -82,52 +82,5 @@ class Admin::EventParticipantsController < Admin::AdminController
   def load_event_participant
     p params
     @event_participant = @event.event_participants.find(params[:id])
-  end
-
-  def render_csv(filename = nil)
-    filename ||= params[:action]
-    filename += '.csv'
-
-    if request.env['HTTP_USER_AGENT'] =~ /msie/i
-      headers['Pragma'] = 'public'
-      headers["Content-type"] = "text/plain"
-      headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
-      headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
-      headers['Expires'] = "0"
-    else
-      headers["Content-Type"] ||= 'text/csv'
-      headers["Content-Disposition"] = "attachment; filename=\"#{filename}\""
-    end
-
-    # todo: add column separator to application config
-    events_csv = FasterCSV.generate :col_sep => ';' do |csv|
-      # header row
-      csv << [t('formtastic.labels.event.code'), 
-        t('formtastic.labels.event.name'),
-        t('formtastic.labels.event.max_participants'),        
-        t('formtastic.labels.event.current_participants'),        
-        t('formtastic.labels.event.vacancies'),        
-        t('formtastic.labels.event_participant.firstname'),
-        t('formtastic.labels.event_participant.lastname'),
-        t('formtastic.labels.event_participant.email'),
-        t('formtastic.labels.event_participant.phone'),
-        t('formtastic.labels.event_participant.notes')
-      ]
-      # data rows
-      @event_participants.each do |participant|
-        csv << [participant.event.code,
-          participant.event.name,
-          participant.event.max_participants,
-          participant.event.current_participants,
-          participant.event.vacancies,
-          participant.firstname,
-          participant.lastname,
-          participant.email,
-          participant.phone,
-          participant.notes
-        ]
-      end
-    end
-    send_data(events_csv, :type => 'text/csv', :filename => filename)
   end
 end
--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,4 @@
 class Admin::EventsController < Admin::AdminController
-
   filter_resource_access :additional_collection => :map, :attribute_check => true
   filter_access_to [:new, :show, :create, :edit, :update], :require => :manage
 
@@ -8,8 +7,8 @@ class Admin::EventsController < Admin::AdminController
     respond_to do |format|
       format.html { @events = @search.paginate(:page => params[:page]) }
       format.xml { render :xml => @search.all }
-      format.csv { @events = @search.all; render_csv("events-#{Time.now.strftime("%Y%m%d")}") }
-      format.xls { @events = @search.all }
+      format.csv { @events = @search.all; @filename = "events-#{Time.now.strftime("%Y%m%d")}.csv" }
+      format.xls { @events = @search.all; @filename = "events-#{Time.now.strftime("%Y%m%d")}.xls" }
     end
   end
 
@@ -22,8 +21,8 @@ class Admin::EventsController < Admin::AdminController
     respond_to do |format|
       format.html
       format.xml  { render :xml => @event }
-      format.csv  { render_csv("event-#{@event.code}") }
-      format.xls
+      format.csv { @filename = "event-#{@event.code}-#{Time.now.strftime("%Y%m%d")}.csv" }
+      format.xls { @filename = "event-#{@event.code}-#{Time.now.strftime("%Y%m%d")}.xls" }
     end
   end
 
@@ -46,74 +45,5 @@ class Admin::EventsController < Admin::AdminController
 
   def load_event
     @event = Event.can_manage(@current_user).find(params[:id])
-  end
-
-  def render_csv(filename = nil)
-    filename ||= params[:action]
-    filename += '.csv'
-
-    if request.env['HTTP_USER_AGENT'] =~ /msie/i
-      headers['Pragma'] = 'public'
-      headers["Content-type"] = "text/plain"
-      headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
-      headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
-      headers['Expires'] = "0"
-    else
-      headers["Content-Type"] ||= 'text/csv'
-      headers["Content-Disposition"] = "attachment; filename=\"#{filename}\""
-    end
-    
-    if @event
-      @events = Array.new
-      @events << @event
-    end
-
-    # todo: add column separator to application config
-    events_csv = FasterCSV.generate :col_sep => ';' do |csv|
-      # header row
-      csv << [t('formtastic.labels.event.code'), 
-        t('formtastic.labels.event.name'),
-        t('formtastic.labels.event.event_type'),
-        t('formtastic.labels.event.location_address_street'),
-        t('formtastic.labels.event.meta_aim_description'),
-        t('formtastic.labels.event.meta_job_description'),
-        t('formtastic.labels.event.max_participants'),
-        t('formtastic.labels.event.current_participants'), 
-        t('formtastic.labels.event.begins_at'),
-        t('formtastic.labels.event.ends_at'),
-        t('formtastic.labels.event.meta_bring_with_you'),
-        t('formtastic.labels.event.meta_provided_for_participiants'),
-        t('formtastic.labels.event.languages'),
-        t('formtastic.labels.event.gathering_location'),
-        t('formtastic.labels.event.notes'),
-        t('formtastic.labels.event.meta_subject_owner'),
-        t('formtastic.labels.event.meta_subject_protegee'),
-        t('formtastic.labels.event.meta_subject_heritage_number'),
-        t('formtastic.labels.event.status')]
-      # data rows
-      @events.each do |event|
-        csv << [event.code,
-          event.name,
-          event.event_type.name,
-          event.location_address,
-          event.meta_aim_description,
-          event.meta_job_description,
-          event.max_participants,
-          event.current_participants,
-          event.begins_at,
-          event.ends_at,
-          event.meta_bring_with_you,
-          event.meta_provided_for_participiants,
-          event.languages.collect(&:name) * ' ',
-          event.gathering_location,
-          event.notes,
-          event.meta_subject_owner,
-          event.meta_subject_protegee,
-          event.meta_subject_heritage_number,
-          t("formtastic.labels.event.statuses.#{event.status}")
-        ]
-      end
-    end
-    send_data(events_csv, :type => 'text/csv', :filename => filename)
   end
 end
